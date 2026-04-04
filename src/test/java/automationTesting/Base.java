4eaab924590ab663ec1ab74c181402f8ba18f485
jayas <jayasharma14nov@gmail.com>
2026-04-03 21:03:33 -0700

    changes in base class

--- src/test/java/automationTesting/Base.java
+++ src/test/java/automationTesting/Base.java
@@ -1,19 +1,29 @@
 package automationTesting;
 
+import java.time.Duration;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 
 public class Base {
 
 	WebDriver driver;
+	WebDriverWait wait;
+	
 
 	@BeforeMethod
 	public void doBefore() {
 
 		driver = new ChromeDriver();
 
+		wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		
+		
 	}
 
 	@AfterMethod
